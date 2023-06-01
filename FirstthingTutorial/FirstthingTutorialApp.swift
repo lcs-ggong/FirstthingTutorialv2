6782c55628a3cd39a42df91aa32b37788c0522d3
Grace Gong <ggong@lcs.on.ca>
2023-06-01 14:20:55 -0400

    Initial Commit

---
+++ FirstthingTutorial/FirstthingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstthingTutorialApp.swift
+//  FirstthingTutorial
+//
+//  Created by Grace Gong on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct FirstthingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
